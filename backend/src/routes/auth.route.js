2d95d815e73abada76c09111686faff2662a5d83
harshitxrajput <harshitxrajput@gmail.com>
2025-04-12 23:57:26 +0530

    started the project

---
+++ backend/src/routes/auth.route.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.post('/signup', (req, res) => {
+
+});
+
+router.post('/login', (req, res) => {
+
+});
+
+router.get('/logout', (req, res) => {
+
+});
+
+export default router;
