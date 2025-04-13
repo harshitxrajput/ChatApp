33a9a2040542552045b3d5042e9046980619f578
harshitxrajput <harshitxrajput@gmail.com>
2025-04-14 01:10:20 +0530

    completed project

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,8 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
 
+import path from 'path';
+
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 
@@ -11,6 +13,7 @@ import { connectToDB } from './lib/db.js';
 import { app, server } from './lib/socket.js';
 
 const PORT=process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ limit: '5mb', extended: true }));
@@ -22,6 +25,14 @@ app.use(cors({
 
 app.use('/api/auth', authRoutes);
 app.use('/api/message', messageRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    })
+}
 
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
