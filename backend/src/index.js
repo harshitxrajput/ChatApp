01980411290bc5084586cdd00defafc27e7e31e7
harshitxrajput <harshitxrajput@gmail.com>
2025-04-13 14:58:19 +0530

    started frontend, created signupPage

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -14,6 +15,10 @@ const PORT=process.env.PORT;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/message', messageRoutes);
